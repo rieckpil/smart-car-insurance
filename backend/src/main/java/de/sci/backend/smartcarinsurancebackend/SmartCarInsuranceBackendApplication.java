4a457ab9bb264ec5b6b775a413cf361c18874d42
Philip Riecks <philip@riecks.io>
2018-07-10 15:28:26 +0200

    add initial backend

---
+++ backend/src/main/java/de/sci/backend/smartcarinsurancebackend/SmartCarInsuranceBackendApplication.java
@@ -0,0 +1,12 @@
+package de.sci.backend.smartcarinsurancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCarInsuranceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCarInsuranceBackendApplication.class, args);
+    }
+}
